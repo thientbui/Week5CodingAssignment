b036de90a8ef92d75eaa6930e98ddac778e12b28
Thien Bui <thientanbui1@gmail.com>
2022-07-30 23:28:21 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,9 @@
+package JavaWeek5; 
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ AsteriskLogger.java
@@ -0,0 +1,9 @@
+
+public class AsteriskLogger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
